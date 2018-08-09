841265f1cf8b0b7573795f500fcfd8ba51027125
Aran MH <dkfks457@ajou.ac.kr>
2018-08-09 23:59:13 +0900

    0809

---
+++ Unity/Hierarchy/Assets/Scripts/SphereController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+
+    private float speed = 10.0f;
+    private bool got = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GameObject child = GameObject.Find("Cube") as GameObject;
+            if (!got)
+            {
+                child.transform.parent = this.transform;
+                got = true;
+            }else
+            {
+                child.transform.parent = null;
+                got = false;
+            }
+        }
+    }
+}
--- Unity/Unity Chan/Assets/Scripts/MainController.cs
+++ Unity/Unity Chan/Assets/Scripts/MainController.cs
@@ -5,10 +5,20 @@ using UnityEngine;
 public class MainController : MonoBehaviour {
 
     public Animator animator;
+    public Rigidbody rigidbody;
 
-	// Use this for initialization
-	void Start () {
+    private float h;
+    private float v;
+
+    private float moveX;
+    private float moveZ;
+    private float speedH = 50F;
+    private float speedZ = 80F;
+
+    // Use this for initialization
+    void Start () {
         animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
 	}
 
     // Update is called once per frame
@@ -17,6 +27,46 @@ public class MainController : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             animator.Play("JUMP00", -1, 0);
+        }
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        animator.SetFloat("h", h);
+        animator.SetFloat("v", v);
+
+        moveX = h * speedH * Time.deltaTime;
+        moveZ = v * speedZ * Time.deltaTime;
+
+        if (moveZ <= 0)
+        {
+            moveX = 0;
+        }
+
+        rigidbody.velocity = new Vector3(moveX, 0, moveZ);
+    }
+    private void OnCollisionEnter(Collision collision)//충돌 시점에서 작동
+    {
+        if (collision.collider.tag == "Cube")
+        {
+            Debug.Log("충돌 감지");
+            animator.Play("DAMAGED01", -1, 0);
+            this.transform.Translate(Vector3.back * speedZ * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)//충돌이 진행되는 동안 작동
+    {
+        if (collision.collider.tag == "Cube")
+        {
+            Debug.Log("충돌 유지");
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)//충돌이 종료되면 작동
+    {
+        if (collision.collider.tag == "Cube")
+        {
+            Debug.Log("충돌 종료");
         }
     }
 }
