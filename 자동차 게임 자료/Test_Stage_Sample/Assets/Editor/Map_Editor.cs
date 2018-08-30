3e27f7d5aa42fc781508e831dcae430cc288a9d8
Aran MH <dkfks457@ajou.ac.kr>
2018-08-30 18:46:14 +0900

    맵을 구성할 기본적인 타일 맵 구현, 자동차의 움직임 구현

---
+++ 자동차 게임 자료/Test_Stage_Sample/Assets/Editor/Map_Editor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(Map_Generator))]
+public class Map_Editor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Map_Generator map = target as Map_Generator;
+
+        map.GenerateMap();
+    }
+
+}
---
+++ 자동차 게임 자료/Test_Stage_Sample/Assets/Scripts/Car_Controler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car_Controler : MonoBehaviour
+{
+    private float speedZ = 75F;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        {
+            this.transform.Translate(Vector3.left * speedZ * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+        {
+            this.transform.Translate(Vector3.forward * speedZ * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            this.transform.Translate(Vector3.back * speedZ * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            this.transform.Translate(Vector3.right * speedZ * Time.deltaTime);
+        }
+    }
+}
